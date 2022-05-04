43151e837cec097c348e208fa835fb591a2ae771
Gregory Schare <grschare1315@gmail.com>
2022-05-03 21:59:59 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,29 @@
+def pick_random_article():
+    pass
+
+def get_article_text(article_id):
+    pass
+
+def utf_to_ascii(text):
+    pass
+
+def load_dictionary():
+    pass
+
+def check_word(word, dictionary, unguessed):
+    pass
+
+def highlight(word):
+    pass
+
+def display():
+    pass
+
+def play():
+    pass
+
+def main():
+    play()
+
+if __name__ == '__main__':
+    main()
